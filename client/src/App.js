98e3b98a3710fd6ef54b1994950534939e89c5c1
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-06 17:00:13 -0700

    moved to impicit grants

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import * as $ from 'jquery';
 import './App.css';
+
+const authEndpoint = 'https://accounts.spotify.com/authorize';
+const clientId = "8beaa14c429347dc96de7ca4c2434e11";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+            className="btn btn--loginApp-link"
+            href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+          >
+            Login to Spotify
         </a>
       </header>
     </div>
