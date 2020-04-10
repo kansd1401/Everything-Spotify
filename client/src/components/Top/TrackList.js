e072efe830f1ce9185e6e82b4ade2cf01202c71a
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-09 18:41:31 -0700

    added varibles scss:

--- client/src/components/Top/TrackList.js
+++ client/src/components/Top/TrackList.js
@@ -4,8 +4,9 @@ import Track from './Track'
 export default function TrackList(props) {
   return (
     <div className="list">
-     {props.tracks.map(track => {
+     {props.tracks.map((track, index) => {
       return <Track
+        key={index+1}
         cover={track.album.images[1].url}
         artists={track.artists}
         album={track.album.name}
