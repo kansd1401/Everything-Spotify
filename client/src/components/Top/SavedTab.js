8dbe4d15cb2ec2cf7c9319973edd9a41452ece1d
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-14 14:50:29 -0700

    ready to test data tab

--- client/src/components/Top/SavedTab.js
+++ client/src/components/Top/SavedTab.js
@@ -1,7 +1,10 @@
 import React,{useState, useEffect} from 'react';
 import * as $ from 'jquery';
+import ListItem from './ListItem'
+import AddedChart from './AddedChart'
 
-const ages = [{name: "Last Month", time_range: "short_term"},{name: "Last 6 Months", time_range: "medium_term"},{name: "All Time", time_range: "long_term"}]
+
+const tabs = [{name: "Weekday", time_range: "short_term"},{name: "Depressed", time_range: "medium_term"},{name: "Danceability", time_range: "long_term"}]
 
 const getData = (token) => {
   let savedTracks = [];
@@ -33,7 +36,8 @@ const getData = (token) => {
           }
         });
       }
-      const dataSet = {savedTracks: savedTracks,dataWeekday: getDataForWeekday(savedTracks)}
+      console.log(savedTracks)
+      const dataSet = {savedTracks: savedTracks,weekday: getDataForWeekday(savedTracks)}
       console.log(dataSet)
       return dataSet
     }
@@ -41,11 +45,19 @@ const getData = (token) => {
 }
 
 const getDataForWeekday = (tracks) => {
-
+  const dataWeekday = [{day: "Sunday", count: 0, percentage:0},{day: "Monday", count: 0, percentage:0},{day: "Tuesday", count: 0, percentage:0},{day: "Wednesday", count: 0, percentage:0},{day: "Thursday", count: 0, percentage:0},{day: "Friday", count: 0, percentage:0},{day: "Saturday", count: 0, percentage:0}]
+  tracks.forEach((track) => {
+    let d = new Date(track.added_at)
+    dataWeekday[d.getDay()].count++
+  })
+  dataWeekday.forEach((day) => {
+    day.percentage = (day.count/tracks.length)*100
+  })
+  return dataWeekday
 }
 
 export default function SavedTab(props) {
-  const [age, setAge] = useState(ages[0].name)
+  const [selected, setSelected] = useState(tabs[0].name)
   const [data, setData] = useState("")
 
   
@@ -57,7 +69,12 @@ export default function SavedTab(props) {
 
   return (
       <section className="top">
+        <div className="age-list">
+          {tabs.map((x,index) => {
+            return <ListItem key={index} name={x.name} selected={selected} setAge={setSelected}/>})}
+        </div>
         <div>
+          {data !== "" && selected === "Weekday" && <AddedChart data={data.weekday}/>}
         </div>
       </section>
   );
