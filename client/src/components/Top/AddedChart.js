5784ac8ad02649601f475771994aafc472abaefb
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-14 16:13:01 -0700

    chart component added for weekday

--- client/src/components/Top/AddedChart.js
+++ client/src/components/Top/AddedChart.js
@@ -3,13 +3,27 @@ import {Bar} from 'react-chartjs-2';
 
 export default function TracksTab(props) {
   return (
-    <div>
+    <div className="chart">
       <Bar data={{
         labels: props.data.map((x) => x.day),
         datasets: [{
-        label: "My First dataset",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
+        label: "Percentage of Saved Tracks",
+        backgroundColor: [
+            'rgba(255, 99, 132)',
+            'rgba(54, 162, 235)',
+            'rgba(255, 206, 86)',
+            'rgba(75, 192, 192)',
+            'rgba(153, 102, 255)',
+            'rgba(255, 159, 64)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132)',
+          'rgba(54, 162, 235)',
+          'rgba(255, 206, 86)',
+          'rgba(75, 192, 192)',
+          'rgba(153, 102, 255)',
+          'rgba(255, 159, 64)'
+        ],
         data: props.data.map((x) => x.percentage),
         }]
     }}/>
