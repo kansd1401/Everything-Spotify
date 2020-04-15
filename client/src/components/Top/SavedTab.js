7f5634661916013747f433a4f31339719f9d02e3
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-14 19:27:02 -0700

    debugging having trouble with requesting several tracks

--- client/src/components/Top/SavedTab.js
+++ client/src/components/Top/SavedTab.js
@@ -53,24 +53,27 @@ export default function SavedTab(props) {
           });
         }
         for(let i = 0; i < savedTracks.length+100; i+=100){
+          let ids = savedTracks.map((track,index) => {
+            if(index < (i+100)){
+              return track.track.id
+            }
+          })
           $.ajax({
-            url: "https://api.spotify.com/v1/audio-features",
+            url: "https://api.spotify.com/v1/audio-features/",
             type: "GET",
-            async: false,
-            dataType: 'json',
-            processData: false,
-            data: $.param({ 
-              ids: savedTracks.map((track,index) => {
-                if(index < i+100){
-                  return track.track.id
-                }
-              })
-            }),
+            // async: false,
+            // dataType: "JSON",
+            // processData: false,
+            data: { 
+              ids: ['4JpKVNYnVcJ8tuMKjAj50A','2NRANZE9UCmPAS5XVbXL40']
+            },
             beforeSend: (xhr) => {
               xhr.setRequestHeader("Authorization", "Bearer " + props.token);
             },
             success: (response) => {
               // savedTracks = [...savedTracks,...response.items]
+              console.log(ids[0])
+
               console.log(response)
             }
           });
