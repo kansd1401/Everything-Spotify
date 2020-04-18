794efbcdb2a0982d50501698a551652a264b89b4
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-18 15:39:36 -0700

    refactored code in chart component

--- client/src/components/Top/Chart.js
+++ client/src/components/Top/Chart.js
@@ -50,29 +50,6 @@ export default function Chart(props) {
         data: props.data.map((x) => x.percentage),
         }]
     }}/>}
-    {props.chartType === "mean" && <Bar data={{
-        labels: Object.keys(props.data),
-        datasets: [{
-        label: "Percentage of Saved Tracks",
-        backgroundColor: [
-            'rgba(255, 99, 132)',
-            'rgba(54, 162, 235)',
-            'rgba(255, 206, 86)',
-            'rgba(75, 192, 192)',
-            'rgba(153, 102, 255)',
-            'rgba(255, 159, 64)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132)',
-          'rgba(54, 162, 235)',
-          'rgba(255, 206, 86)',
-          'rgba(75, 192, 192)',
-          'rgba(153, 102, 255)',
-          'rgba(255, 159, 64)'
-        ],
-        data: Object.values(props.data),
-        }]
-    }}/>}
     </div>
   );
 }
