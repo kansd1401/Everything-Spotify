03eb109e4933e2b62ce74c2ad4d51d5a906f9955
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-04-18 13:15:00 -0700

    added a helper fuction fiel to clean up the compoenent mess

--- client/src/components/Top/SavedTab.js
+++ client/src/components/Top/SavedTab.js
@@ -2,72 +2,11 @@ import React,{useState, useEffect} from 'react';
 import * as $ from 'jquery';
 import ListItem from './ListItem'
 import Chart from './Chart'
+import {getDataForDanceability, getDataForEnergy, getDataForValence, getDataForWeekday} from "../../helpers/dataConversion"
 
 
 const tabs = [{name: "Weekday", time_range: "short_term"},{name: "Depressed", time_range: "medium_term"},{name: "Dance", time_range: "long_term"},{name: "Energy", time_range: "long_term"}]
 
-const getDataForWeekday = (tracks) => {
-  const dataWeekday = [{day: "Sunday", count: 0, percentage:0},{day: "Monday", count: 0, percentage:0},{day: "Tuesday", count: 0, percentage:0},{day: "Wednesday", count: 0, percentage:0},{day: "Thursday", count: 0, percentage:0},{day: "Friday", count: 0, percentage:0},{day: "Saturday", count: 0, percentage:0}]
-  tracks.forEach((track) => {
-    let d = new Date(track.added_at)
-    dataWeekday[d.getDay()].count++
-  })
-  dataWeekday.forEach((day) => {
-    day.percentage = (day.count/tracks.length)*100
-  })
-  return dataWeekday
-}
-
-const getDataForValence = (tracks) => {
-  const data = [{day: "Sad", count: 0, percentage:0},{day: "OK", count: 0, percentage:0},{day: "Happy", count: 0, percentage:0}]
-  tracks.forEach((x) => {
-    if(x.track.valence < 0.33){
-      data[0].count++
-    }else if(x.track.valence > 0.66){
-      data[2].count++
-    }else{
-      data[1].count++
-    }
-  })
-  data.forEach((day) => {
-    day.percentage = (day.count/tracks.length)*100
-  })
-  return data
-}
-
-const getDataForEnergy = (tracks) => {
-  const data = [{day: "Low", count: 0, percentage:0},{day: "OK", count: 0, percentage:0},{day: "High", count: 0, percentage:0}]
-  tracks.forEach((x) => {
-    if(x.track.energy < 0.33){
-      data[0].count++
-    }else if(x.track.energy > 0.66){
-      data[2].count++
-    }else{
-      data[1].count++
-    }
-  })
-  data.forEach((day) => {
-    day.percentage = (day.count/tracks.length)*100
-  })
-  return data
-}
-
-const getDataForDanceability = (tracks) => {
-  const data = [{day: "Chill Music", count: 0, percentage:0},{day: "Do what you want", count: 0, percentage:0},{day: "Party Song", count: 0, percentage:0}]
-  tracks.forEach((x) => {
-    if(x.track.danceability < 0.60){
-      data[0].count++
-    }else if(x.track.danceability > 0.80){
-      data[2].count++
-    }else{
-      data[1].count++
-    }
-  })
-  data.forEach((day) => {
-    day.percentage = (day.count/tracks.length)*100
-  })
-  return data
-}
 
 export default function SavedTab(props) {
   const [selected, setSelected] = useState(tabs[0].name)
---
+++ client/src/helpers/dataConversion.js
@@ -0,0 +1,64 @@
+const getDataForWeekday = (tracks) => {
+  const dataWeekday = [{day: "Sunday", count: 0, percentage:0},{day: "Monday", count: 0, percentage:0},{day: "Tuesday", count: 0, percentage:0},{day: "Wednesday", count: 0, percentage:0},{day: "Thursday", count: 0, percentage:0},{day: "Friday", count: 0, percentage:0},{day: "Saturday", count: 0, percentage:0}]
+  tracks.forEach((track) => {
+    let d = new Date(track.added_at)
+    dataWeekday[d.getDay()].count++
+  })
+  dataWeekday.forEach((day) => {
+    day.percentage = (day.count/tracks.length)*100
+  })
+  return dataWeekday
+}
+
+const getDataForValence = (tracks) => {
+  const data = [{day: "Sad", count: 0, percentage:0},{day: "OK", count: 0, percentage:0},{day: "Happy", count: 0, percentage:0}]
+  tracks.forEach((x) => {
+    if(x.track.valence < 0.33){
+      data[0].count++
+    }else if(x.track.valence > 0.66){
+      data[2].count++
+    }else{
+      data[1].count++
+    }
+  })
+  data.forEach((day) => {
+    day.percentage = (day.count/tracks.length)*100
+  })
+  return data
+}
+
+const getDataForEnergy = (tracks) => {
+  const data = [{day: "Low", count: 0, percentage:0},{day: "OK", count: 0, percentage:0},{day: "High", count: 0, percentage:0}]
+  tracks.forEach((x) => {
+    if(x.track.energy < 0.33){
+      data[0].count++
+    }else if(x.track.energy > 0.66){
+      data[2].count++
+    }else{
+      data[1].count++
+    }
+  })
+  data.forEach((day) => {
+    day.percentage = (day.count/tracks.length)*100
+  })
+  return data
+}
+
+const getDataForDanceability = (tracks) => {
+  const data = [{day: "Chill Music", count: 0, percentage:0},{day: "Do what you want", count: 0, percentage:0},{day: "Party Song", count: 0, percentage:0}]
+  tracks.forEach((x) => {
+    if(x.track.danceability < 0.60){
+      data[0].count++
+    }else if(x.track.danceability > 0.80){
+      data[2].count++
+    }else{
+      data[1].count++
+    }
+  })
+  data.forEach((day) => {
+    day.percentage = (day.count/tracks.length)*100
+  })
+  return data
+}
+
+export {getDataForDanceability, getDataForEnergy, getDataForValence, getDataForWeekday}
