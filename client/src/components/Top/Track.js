d60430d3e6d5f2cd9a7fc7388024e43f0cb4ec0d
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-11 21:20:34 -0700

    working on colors again

--- client/src/components/Top/Track.js
+++ client/src/components/Top/Track.js
@@ -1,19 +1,18 @@
 import React from "react";
 import classnames from "classnames";
 import { Palette } from "color-thief-react";
-
+import {Spinner} from 'react-bootstrap'
 
 export default function Track(props) {
   const artists = props.artists.map((artist) => artist.name).join(", ")
   const trackInfoClass = classnames("track__info", {
     "track__info--explicit": props.explicit
   });
-  const Loading = () => <div>Loading...</div>;
 
   return (
     <Palette src={props.cover} crossOrigin="Anonymous" colorCount={2} format="hex">
     {({ data, loading }) => {
-      if (loading) return <Loading />;
+      if (loading) return <Spinner animation="border"  variant="dark"/>;
       return (
           <article className="track" style={{ background: data[1], color: data[0] }}>
             <img className="track__thumbnail" src={props.cover} alt="Track" />
