664f3e04078e2b3e4681430f42aea70aa46e2da6
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-11 20:42:46 -0700

    cool color thief working

--- client/src/components/Top/Track.js
+++ client/src/components/Top/Track.js
@@ -1,22 +1,32 @@
 import React from "react";
 import classnames from "classnames";
+import { Palette } from "color-thief-react";
+
 
 export default function Track(props) {
   const artists = props.artists.map((artist) => artist.name).join(", ")
   const trackInfoClass = classnames("track__info", {
     "track__info--explicit": props.explicit
   });
+  const Loading = () => <div>Loading...</div>;
 
   return (
-    <article className="track">
-      <img className="track__thumbnail" src={props.cover} alt="Track" />
-      <div className={trackInfoClass}>
-        <div className="track__rank">
-          {props.rank}
-        </div>
-        <div className="track__name">{props.name}</div>
-        <div className="track__artists">{artists}</div>
-      </div>
-    </article>
+    <Palette src={props.cover} crossOrigin="Anonymous" colorCount={2} format="hex">
+    {({ data, loading }) => {
+      if (loading) return <Loading />;
+      return (
+          <article className="track" style={{ background: data[1], color: data[0] }}>
+            <img className="track__thumbnail" src={props.cover} alt="Track" />
+            <div className={trackInfoClass}>
+              <div className="track__rank">
+                {props.rank}
+              </div>
+              <div className="track__name">{props.name}</div>
+              <div className="track__artists">{artists}</div>
+            </div>
+          </article>
+          );
+        }}
+      </Palette>
   );
 }
