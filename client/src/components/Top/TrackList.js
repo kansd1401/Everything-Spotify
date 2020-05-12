3d7c8d3d276df37096e54a0ca93d2b4b305a9dfc
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-11 20:56:19 -0700

    pretty stuff

--- client/src/components/Top/TrackList.js
+++ client/src/components/Top/TrackList.js
@@ -8,7 +8,7 @@ export default function TrackList(props) {
       return <Track
         key={index+1}
         rank={index+1}
-        cover={track.album.images[2].url}
+        cover={track.album.images[1].url}
         artists={track.artists}
         name={track.name}
         explicit={track.explicit}
