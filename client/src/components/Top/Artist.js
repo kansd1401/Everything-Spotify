0edf3b3bc94f6c2cd8fc1d3b8c951e9cc55cbedb
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-13 13:00:03 -0700

    changes to artist component to better style it

--- client/src/components/Top/Artist.js
+++ client/src/components/Top/Artist.js
@@ -7,8 +7,10 @@ export default function Artist(props) {
       <img className="artist__thumbnail" src={props.cover} alt="Artist" />
       <div className="artist__info">
         <div className="artist__rank">{props.rank}</div>
-        <div className="artist__name">{props.name}</div>
-        <div className="artist__popularity">Popularity {props.popularity}</div>
+        <div className="artist__side">
+          <div className="artist__name">{props.name}</div>
+          <div className="artist__popularity">Popularity {props.popularity}</div>
+        </div>
       </div>
     </article>
   );
