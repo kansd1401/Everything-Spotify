2f1f28493ad3f940ec7f26e0b9d4fd7f873c9450
Sanjeet Kang <kang.sanjeet1401@gmail.com>
2020-05-13 14:20:16 -0700

    finished styling artists tab

--- client/src/components/Top/Artist.js
+++ client/src/components/Top/Artist.js
@@ -7,7 +7,7 @@ export default function Artist(props) {
       <img className="artist__thumbnail" src={props.cover} alt="Artist" />
       <div className="artist__info">
         <div className="artist__rank">{props.rank}</div>
-        <div className="artist__name">{props.name}</div>
+        <p className="artist__name">{props.name}</p>
         {/* <div className="artist__popularity">Popularity {props.popularity}</div> */}
       </div>
     </article>
